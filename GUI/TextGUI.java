ea8501a0064c5d1a026d0946c5eed8ec64045769
Dmzori <noahhoffer@gmail.com>
2022-06-18 02:38:41 -0600

    Components are on the panel but formating is off

--- GUI/TextGUI.java
+++ GUI/TextGUI.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -21,6 +23,7 @@ public class TextGUI {
     private final GridBagConstraints textFieldConstraint;//the size constraint to be used for the gridbaglayout
     private final GridBagConstraints textAreaConstraint;
     private final Dimension minSize;//the min size of the frame
+    //private final int relative;
     
     public TextGUI()
     {
@@ -34,30 +37,49 @@ public class TextGUI {
         gridBagLayout = new GridBagLayout();
         textFieldConstraint = new GridBagConstraints();//each component should have its own constraint size when added to it's panel unless you want them to act the same at all times 
         textAreaConstraint = new GridBagConstraints();
+        //relative = GridBagConstraints.RELATIVE;
         
 
-        //GridBagConstraint stuff
+        //textField gridBagConstraint stuff
+        textFieldConstraint.fill = GridBagConstraints.HORIZONTAL;
+        textFieldConstraint.gridx = 0;
+        textFieldConstraint.gridy = 0;
+        textFieldConstraint.anchor = GridBagConstraints.FIRST_LINE_START;
+        textFieldConstraint.gridwidth = GridBagConstraints.REMAINDER;
+        textFieldConstraint.gridwidth = 5000;
+        textFieldConstraint.insets = new Insets(10, 10, 10, 10);
+        
+        //textArea gridBagConstraint stuff
+        textAreaConstraint.fill = GridBagConstraints.HORIZONTAL;
+        textAreaConstraint.gridx = 0;
+        textAreaConstraint.gridy = GridBagConstraints.RELATIVE;
+        textAreaConstraint.ipady = 40;
+        textAreaConstraint.anchor = GridBagConstraints.LINE_START;
+        textAreaConstraint.gridwidth = GridBagConstraints.REMAINDER;
+        //textAreaConstraint.gridwidth = 5;
         
         //textField stuff
-        
+        textField.setEditable(true);
         
         //textArea stuff
         textArea.setEditable(false);
-
+        
+        
+        //panel stuff
+        mainPanel.setLayout(gridBagLayout);
+        mainPanel.add(textField, textFieldConstraint);
+        mainPanel.add(textArea, textAreaConstraint);
+        mainPanel.setBackground(Color.BLACK);
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
         
         //frame stuff
         mainFrame.add(mainPanel);
         mainFrame.setMinimumSize(minSize);
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainFrame.pack();
-        mainFrame.getContentPane().setBackground(Color.BLACK);
+        //mainFrame.pack();
+        mainFrame.getContentPane().setBackground(Color.red);
         mainFrame.setVisible(true);
         mainFrame.setTitle("Text Engine");
-        
-        //panel stuff
-        //mainPanel.setLayout(gridBagLayout);
-        mainPanel.add(textField, textFieldConstraint);
-        mainPanel.add(textArea, textAreaConstraint);
         
     }
 }
